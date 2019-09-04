aff504a074905e7d9f3a0850d59b5f3b80d9ec03
RYuriy <radov.yuriy@ukr.net>
2019-09-05 00:01:57 +0300

    + SwapServise's methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-
 class SwapServise {
     _apiBase = 'https://swapi.co/api';
     _peopleUrl = this._apiBase + '/people/';
+    _planetsUrl = this._apiBase + '/planets/';
+    _starshipsUrl = this._apiBase + '/starships/';
 
     async gerResource(url) {
         const resp = await fetch(url);
@@ -14,20 +15,45 @@ class SwapServise {
         return await resp.json();
     }//gerResource
 
-    getAllPeople() {
-        return this.gerResource(this._peopleUrl);
+    async getAllPeople() {
+        const res = await this.gerResource(this._peopleUrl);
+        return res.results;
     }//getAllPeople
 
     getPerson(id) {
         return this.gerResource(this._peopleUrl + id);
     }//getAllPeople
 
+    async getAllPlanets() {
+        const res = await this.gerResource(this._planetsUrl);
+        return res.results;
+    }//getAllPlanets
+
+    getPlanet(id) {
+        return this.gerResource(this._planetsUrl + id);
+    }//getPlanet
+
+    async getAllStarships() {
+        const res = await this.gerResource(this._starshipsUrl);
+        return res.results;
+    }//getAllStarships
+
+    getStarship(id) {
+        return this.gerResource(this._starshipsUrl + id);
+    }//getStarship
+
 }//SwapServise
 
 const swapi = new SwapServise();
 
-swapi.getAllPeople().then((body) => {
-    console.log(body);
+swapi.getAllStarships().then((items) => {
+    items.forEach((item) => {
+        console.log(item.name);
+    });
+});
+
+swapi.getStarship(3).then((item) => {
+    console.log(item);
 });
 
 
